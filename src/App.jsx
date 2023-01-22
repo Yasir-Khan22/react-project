75559a08b0640d71fc341d1b5d5cad22757a6cfb
Yasir-Khan22 <yasirkhanone22@gmail.com>
2023-01-22 21:38:55 +0500

    Store and Provider created in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import './App.css'
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/cart' element={<Cart />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/cart' element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
